1c7e85e74f0f1208e0e2795fdcccc98d8970e9ef
Matheus Tanure <matheus.tanure@iteris.com.br>
2022-09-13 09:50:18 -0300

    remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "tanure-teste"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
