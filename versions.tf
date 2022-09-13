9c535c9f5db6197f49251a08f201df9ce76acd7f
Matheus Tanure <matheus.tanure@iteris.com.br>
2022-09-13 11:11:46 -0300

    credentials

--- versions.tf
+++ versions.tf
@@ -1,20 +1,19 @@
 terraform {
-  /*
+  /*  
   cloud {
-    organization = "tanure-teste"
+     organization = "tanure-teste"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-*/
+  */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.28.0"
     }
   }
-  required_version = ">= 0.14.0"
 }
 
 provider "aws" {
